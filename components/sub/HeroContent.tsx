4d9e819d5a34e279e9821af7887fc47f1730cb4f
Jordan <jordannwright1@gmail.com>
2025-05-07 13:46:07 -0400

    fixed width issue on mobile

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -14,7 +14,7 @@ const HeroContent = () => {
     animate='visible'
     className='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'
     >
-      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+      <div className="h-full w-auto flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
         className='Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]'
         variants={slideInFromTop}
