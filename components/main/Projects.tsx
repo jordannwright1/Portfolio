e65ef629fe5061d0ed6921476c6a3d418aaa3e55
Jordan <jordannwright1@gmail.com>
2025-05-18 17:35:55 -0400

    Added VStream

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -8,7 +8,7 @@ const Projects = () => {
       {
         id: 1,
         src: '/AIFitnessTrainer.png',
-        title: 'AIFitnessTrainer App',
+        title: 'AIFitnessTrainer',
         description: 'AIFitnessTrainer is a fitness app powered by an AI Trainer model built using a Vapi.ai workflow. Users can simulate phone calls with the AI Trainer, which analyzes the conversation to generate personalized diet and workout plans. The app features secure sign-up/sign-in functionality, with a dedicated profile page for users to view and manage their plans. Technologies used: Vapi.ai Workflow and API, Gemini AI API, Next.js, Convex, Clerk Authentication, and ShadCN Library.  Guest Email: s26887809@gmail.com, Guest Password: AIFitnessTrainer',
         link: 'https://ai-fitness-trainer-delta.vercel.app'
       },
@@ -18,6 +18,13 @@ const Projects = () => {
         title: 'Movie App',
         description: 'A movie browsing app that fetches data from an external API to display a curated selection of movies. It features a search bar for finding specific titles and allows users to mark movies as favorites. Favorited movies are saved and displayed on a dedicated favorites page for easy access. Technologies used: The Movie Database API, React.',
         link: 'https://react-movie-project-tau.vercel.app'
+      },
+      {
+        id: 3,
+        src: '/VStream.png',
+        title: 'VStream',
+        description: 'VStream is a real-time communication platform featuring one-on-one and group video calls, chat functionality, reactions, and friend requests. Built using the MERN stack, the app leverages GetStream.io for seamless chat and video calling integration. Users can connect with friends, join groups, and send real-time reactions, while the friend request feature enhances social interactivity. The app ensures a secure and responsive user experience, incorporating authentication and dynamic updates. Technologies used: MongoDB, Express.js, React.js, Node.js, GetStream.io API, Stream Chat SDK, Tailwind CSS, and React Query. Guest Email: john@example.com, Guest Password: 123456',
+        link: 'https://vstream-hgsa.onrender.com'
       }
     ];
 
